66161721104f58d804a2f912cee5533bc931e5e0
Riley <riley.weaselfat@uleth.ca>
2021-11-06 11:01:09 -0600

    Login working

--- src/main.php
+++ src/main.php
@@ -1,7 +1,8 @@
 <html>
     <h1> Railroad system </h1>
-    
+
     <ul>
         <li><a href="insert_train.php">Insert a Train</a></li>
-    </ul>   
+    </ul>
+    <p><a href="logout.php">Logout</a></p>
 </html>
--- src/setpass.php
+++ src/setpass.php
@@ -1,9 +1,9 @@
 <?php
-
-    $conn = new mysqli("vconroy.cs.uleth.ca", $_POST["username"], $_POST["password"]);
+    #$conn = new mysqli("vconroy.cs.uleth.ca", $_POST["username"], $_POST["password"]);
+    $conn = new mysqli("localhost", $_POST["username"], $_POST["password"]);
     if($conn->connect_errno) {
         echo "$conn->connect_errno";
-        echo "<h1>Invalid username or password!<\h1><p><a href=\"index.php\">Try Again</a></p>";
+        echo "<h1>Invalid username or password!</h1><p><a href=\"index.php\">Try Again</a></p>";
         exit;
     }
     $username = $_POST["username"];
