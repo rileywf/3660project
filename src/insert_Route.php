6ceb68989409b5f0fa12e71675ccc5f9b9696f22
Riley <riley.weaselfat@uleth.ca>
2021-11-13 18:10:38 -0700

    Adding routes work

---
+++ src/insert_Route.php
@@ -0,0 +1,9 @@
+<html>
+    <h1>Add a new Route</h1>
+    <body>
+        <form action="insertRoute.php" method=post>
+        Name of Route: <input type=text name="rname" size=20><br><br>
+        Supported Trains: <input type=text name="sname" size=20><br><br>
+        <input type=submit name="Submit" value="Insert"></form>
+    </body>
+</html>
--- src/setpass.php
+++ src/setpass.php
@@ -1,7 +1,6 @@
 <?php
-phpinfo();
-    #$conn = new mysqli("vconroy.cs.uleth.ca", $_POST["username"], $_POST["password"]);
-    $conn = new mysqli("localhost", $_POST["username"], $_POST["password"]);
+    $conn = new mysqli("vconroy.cs.uleth.ca", $_POST["username"], $_POST["password"]);
+    #$conn = new mysqli("localhost", $_POST["username"], $_POST["password"]);
     if($conn->connect_errno) {
         echo "$conn->connect_errno";
         echo "<h1>Invalid username or password!</h1><p><a href=\"index.php\">Try Again</a></p>";
@@ -10,8 +9,9 @@ phpinfo();
     $username = $_POST["username"];
     $password = $_POST["password"];
 
+
     setcookie("username", $username, time()+3600);
     setcookie("password", $password, time()+3600);
 
     header('Location:main.php');
-    ?>
+?>
