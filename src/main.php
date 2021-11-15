1784cb6e79c9d49effeb322ef7a5dcee36815849
Cody <hodgec@uleth.ca>
2021-11-15 12:02:02 -0700

    looking good

--- src/main.php
+++ src/main.php
@@ -1,13 +1,26 @@
 <html>
     <h1> Railroad system </h1>
 
+    <h2> Insert </h2>
     <ul>
         <li><a href="insert_train.php">Insert a Train</a></li>
-        <li><a href="insert_Route.php">Insert a Route</a></li>
-        <li><a href="query_Route.php">Query a Route</a></li>
-        <li><a href="delete_Route.php">Delete a Route</a></li>
-        <li><a href="update_Route.php">Update a Route</a></li>
-
+        <li><a href="insert_Route.php">Insert a Route</a></li><br><br>
     </ul>
+
+    <h2> Query </h2>
+    <ul>
+        <li><a href="query_Route.php">Query a Route</a></li><br><br>
+    </ul>
+
+    <h2> Update </h2>
+    <ul>
+    <li><a href="update_Route.php">Update a Route</a></li><br><br>
+    </ul>
+
+    <h2> Delete </h2>
+    <ul>
+        <li><a href="delete_Route.php">Delete a Route</a></li><br><br>
+    </ul>
+
     <p><a href="logout.php">Logout</a></p>
 </html>
