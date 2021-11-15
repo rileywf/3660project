ec858c0071474917c1f25e186b9f82502228a7c0
Michael Wyn <19212818+esk205@users.noreply.github.com>
2021-11-14 21:22:19 -0700

    Add files via upload

--- src/query_Route.php
+++ src/query_Route.php
@@ -2,8 +2,9 @@
 	<h1>Query Route</h1>
 	<body>
 		<form action="queryRoute.php" method=post>
-		Route Name: <input type=text name="tname" size=20><br><br>
-
+		Route Name: <input type=text name="rname" size=20><br><br>
+		Route ID: <input type=text name="IDname" size=20><br><br>
+		Supported Trains: <input type=text name="sname" size=20><br><br>
 		<input type=submit name="Submit" value="Query"></form>
 	</body>
 </html> 
