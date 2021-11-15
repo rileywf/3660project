b56b67063a00517059f1959ca47e765a586bf0b2
Cody <hodgec@uleth.ca>
2021-11-15 09:42:04 -0700

    added stuff to main

--- src/main.php
+++ src/main.php
@@ -4,6 +4,9 @@
     <ul>
         <li><a href="insert_train.php">Insert a Train</a></li>
         <li><a href="insert_Route.php">Insert a Route</a></li>
+        <li><a href="query_Route.php">Query a Route</a></li>
+        <li><a href="delete_Route.php">Delete a Route</a></li>
+        <li><a href="update_Route.php">Update a Route</a></li>
 
     </ul>
     <p><a href="logout.php">Logout</a></p>
