5ce2e975036ab2e08cbf2bcc5bb36bade47a523a
Michael Wyn <19212818+esk205@users.noreply.github.com>
2021-11-15 20:00:06 -0700

    Add files via upload

--- src/updateRoute.php
+++ src/updateRoute.php
@@ -1,17 +1,49 @@
 <?php
+if(isset($_COOKIE["username"])){
 
- if(isset($_COOKIE["username"])) {
-    $username = $_COOKIE["username"];
-    $password = $_COOKIE["password"];
+echo "<form action=\"updateRoute2.php\" method=post>";
 
-    $conn = new mysqli("vconroy.cs.uleth.ca",$username,$password,$username);
-    if($mysqli->connect_errno) {
-      echo "Connection Error!";
-      exit;
-    }
-    
-    echo "<a href=\"main.php\">Return</a> to Home Page.";
-  } else {
-    echo "<h3>You are not logged in!</h3><p> <a href=\"index.php\">Login First</a></p>";
-  }
+	$username = $_COOKIE["username"];
+	$password = $_COOKIE["password"];	
+
+	$conn = new mysqli("vconroy.cs.uleth.ca",$username,$password,$username);
+	if($conn->connect_errno)
+	{
+	   echo "Connection Problem!";
+	   exit; 
+	}
+	
+	$sql = "select * from ROUTES 
+	    where name='$_POST[rname]'
+	    and typesOfTrain='$_POST[sname]'
+	    and ID='$_POST[IDname]'";
+
+	$result = $conn->query($sql);
+	if(!$result)
+	{
+	   echo "Problem executing select!";
+	   exit; 
+	}
+
+     // ('$_POST[IDname]','$_POST[rname]','$_POST[sname]')"; AND IDname='$_POST[IDname]'
+
+        if($result->num_rows!= 0)
+	{
+	   $rec=$result->fetch_assoc(); 
+	   echo "Route Name: <input type=text name=\"rname\" value=\"$rec[rname]\"><br><br>";
+	   echo "Supported Trains: <input type=text typesOfTrain=\"loc\" value=\"$rec[sname]\"><br><br>";
+	   echo "Route ID: <input type=text name=\"ID\" value=\"$rec[IDname]\"><br><br>";
+	   echo "<input type=hidden name=\"oldname\" value=\"$_POST[rname]\">";
+	   echo "<input type=submit name=\"submit\" value=\"Update\">"; 
+	}
+	else
+	{
+		echo "<p>Umm...you may want to enter some data. ;) </p>"; 
+	}
+
+	echo "</form>";
+} else {
+   echo "<h3>You are not logged in!</h3><p> <a href=\"index.php\">Login First</a></p>"; 
+
+}
 ?>
