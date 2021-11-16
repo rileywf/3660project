cd3d124592cc8eaf52744268c6eb869e625c6729
Cody <hodgec@uleth.ca>
2021-11-15 20:48:17 -0700

    displays data

--- src/queryRoute.php
+++ src/queryRoute.php
@@ -10,17 +10,30 @@
       echo "Connection Error!";
       exit;
     }
-  
-    $sql = "select * from ROUTES 
+
+    $sql = "select * from ROUTES
 	    where name='$_POST[rname]'
 	    and typesOfTrain='$_POST[sname]'
 	    and ID='$_POST[IDname]'";
-  
-    if($conn->query($sql)) {
-      echo "<h3>Route Found! Here are the Details</h3>";
-      echo "<td>$rec[rname]</td>";
-      echo "<td>$rec[IDname]</td>";
-      echo "<td>$rec[sname]</td>";
+    $result = $conn->query($sql);
+
+      if($conn->query($sql)) {
+      echo "<table border='1' style='width:100%'>
+      <tr>
+      <th>ID</th>
+      <th>Type</th>
+      <th>Name</th>
+      </tr>";
+
+      while($row = mysqli_fetch_array($result))
+      {
+      echo "<tr>";
+      echo "<td>" . $row['ID'] . "</td>";
+      echo "<td>" . $row['typesOfTrain'] . "</td>";
+      echo "<td>" . $row['name'] . "</td>";
+      echo "</tr>";
+      }
+      echo "</table>";
     } else {
       $err = $conn->errono;
       printf("error: %d", $err);
@@ -30,5 +43,3 @@
     echo "<h3>You are not logged in!</h3><p> <a href=\"index.php\">Login First</a></p>";
   }
 ?>
-
-
