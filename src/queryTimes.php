d731a5a9c40917f9d29bdb48cd1979373919ddf9
schl3720 <schmidtbellachl@uleth.ca>
2021-12-04 12:53:30 -0700

    Create pages, add basic framework

---
+++ src/queryTimes.php
@@ -0,0 +1,21 @@
+<?php
+
+if(isset($_COOKIE["username"])) {
+    $username = $_COOKIE["username"];
+    $password = $_COOKIE["password"];
+
+    $conn = new mysqli("vconroy.cs.uleth.ca",$username,$password,$username);
+    if($mysqli->connect_errno) {
+      echo "Connection Error!";
+      exit;
+    }
+
+    $sql =
+
+
+
+
+} else {
+    echo "<h3>You are not logged in!</h3><p> <a href=\"index.php\">Login First</a></p>";
+  }
+?>
