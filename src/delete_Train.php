5b80af9a93142131d36e195fada1e966e692b21c
Riley <riley.weaselfat@uleth.ca>
2021-12-04 16:13:22 -0700

    beep

--- src/delete_Train.php
+++ src/delete_Train.php
@@ -1,5 +1,20 @@
 <html>
+  <head>
+    <title>Railroad System</title>
+  </head>
   <body>
+    <?php
+      if(isset($_COOKIE["username"])) {
+        $username = $_COOKIE["username"];
+        $password = $_COOKIE["password"];
+        $conn = new mysqli("vconroy.cs.uleth.ca", $username, $password, $username);
+
+
+        echo "<form action=\"deleteTrain.php\" method=post>";
+        $sql = "select * from TRAIN";
+        $result = $conn->query($sql);
+      }
+     ?>
 
   </body>
 </html>
