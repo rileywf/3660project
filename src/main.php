1d5224c0ddc238ee6425ce634c7e2830baa58e53
Michael Wynnychuk <m.wynnychuk@uleth.ca>
2021-12-05 15:00:55 -0700

    Updating main.php

--- src/main.php
+++ src/main.php
@@ -22,17 +22,17 @@
     </ul>
     <h2> Update </h2>
     <ul>
-    <li><a href="update_Train.php">Update Train</a></li><br><br>
-    <li><a href="update_Conductor.php">Update Conductor</a></li><br><br>
-    <li><a href="update_Route.php">Update a Route</a></li><br><br>
-    <li><a href="update_Station.php">Update a Station</a></li><br><br>
+      <li><a href="update_Train.php">Update Train</a></li><br><br>
+      <li><a href="update_Conductor.php">Update Conductor</a></li><br><br>
+      <li><a href="update_Route.php">Update a Route</a></li><br><br>
+      <li><a href="update_Station.php">Update a Station</a></li><br><br>
     </ul>
     <h2> Delete </h2>
     <ul>
-        <li><a href="delete_Train.php">Delete a Route</a></li><br><br>
-        <li><a href="delete_Conductor.php">Delete a Route</a></li><br><br>
+        <li><a href="delete_Train.php">Delete a Train</a></li><br><br>
+        <li><a href="delete_Conductor.php">Delete a Conductor</a></li><br><br>
         <li><a href="delete_Route.php">Delete a Route</a></li><br><br>
-        <li><a href="delete_Station.php">Delete a Route</a></li><br><br>
+        <li><a href="delete_Station.php">Delete a Station</a></li><br><br>
     </ul>
     <p><a href="logout.php">Logout</a></p>
 </html>
