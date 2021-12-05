f6d2c4c2f416902657333b9f0f9c7064b5f0a767
Michael Wynnychuk <m.wynnychuk@uleth.ca>
2021-12-05 13:44:11 -0700

    Updating conductor

--- src/update_Conductor.php
+++ src/update_Conductor.php
@@ -3,22 +3,15 @@
 	<body>
 	<h2>Specify Which Conductor to Update</h2>
 		<form action="updateConductor.php" method=post>
-		Specify Conductor Name: <input type=text name="rname" size=20><br><br>
-    <label for="typesOfTrain">Choose a Certification type:</label>
-    <select name="sname" id="Certification">
+		Specify Conductor Name: <input type=text name="Cname" size=20><br><br>
+    Specify Phone Number: <input type=text name="PN" size=20><br><br>
+    Specify Conductor Age: <input type=text name="age" size=20><br><br>
+    <label for="Certification">Choose a Certification type:</label>
+    <select name="Cert" id="Certification">
     <option value="Yes">Yes</option>
     <option value="No">No</option>
   </select> <br> <br>
-		Specify Route ID: <input type=text name="IDname" size=20><br><br>
-		<input type=submit name="Submit" value="Update Route"></form>
+		Specify Conductor ID: <input type=text name="IDname" size=20><br><br>
+		<input type=submit name="Submit" value="Update Conductor"></form>
 	</body>
 </html>
-
-
-    `ID` int(11) NOT NULL AUTO_INCREMENT,
-    `rID` int(11) DEFAULT NULL,
-    `phoneNum` int(11) DEFAULT NULL,
-    `condName` char(255) DEFAULT NULL,
-    `age` int(11) DEFAULT NULL,
-    `Certification` enum('Yes','No') DEFAULT NULL,
-    PRIMARY KEY (`ID`),
