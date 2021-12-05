32688506a442f6b4b1ec3d4b85bdea9fd0480374
Riley <riley.weaselfat@uleth.ca>
2021-12-05 13:12:36 -0700

    Fancy Table

---
+++ src/deleteTrain.php
@@ -0,0 +1,25 @@
+<?php
+
+if(isset($_COOKIE["username"])) {
+  $username = $_COOKIE["username"];
+  $password = $_COOKIE["password"];
+  $conn = new mysqli("vconroy.cs.uleth.ca",$username,$password,$username);
+  $val = $_GET['ID'];
+  $sql = "delete from TRAIN where ID='$val'";
+  if($conn->query($sql)) {
+    echo "<h3>Train Deleted</h3>";
+  } else {
+    $err = $conn->errno;
+    $errtext = $conn->error;
+    if($err == 1451) {
+      echo "<h3>Cannot delete TRAIN $_GET[ID]!</h3>";
+      echo "One or more of $_GET[ID] has trains or conductors assigned to them.";
+    } else {
+      echo "You got error code of $err. $errtext";
+      echo "<br><br><a href=\"main.php\">Return</a> to Home Page.";
+    }
+  }
+  } else {
+      echo "<h3>You are not logged in!</h3><p> <a href=\"index.php\">Login First</a></p>";
+}
+?>
