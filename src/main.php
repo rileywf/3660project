fa8912c6f0475ecb0908f600243b89de5f553334
Riley <riley.weaselfat@uleth.ca>
2021-12-05 23:21:09 -0700

    Let me push up

--- src/main.php
+++ src/main.php
@@ -36,6 +36,7 @@
         <li><a href="delete_Conductor.php">Delete a Conductor</a></li>
         <li><a href="delete_Route.php">Delete a Route</a></li>
         <li><a href="delete_Station.php">Delete a Station</a></li>
+        <li><a href="delete_Time.php">Delete time</a></li>
     </ul>
     <p><a href="logout.php">Logout</a></p>
 </html>
