d0422532b8c6db21cbbd08161df05520dcbf46db
CyberpunkLuna <titaniumqubez@gmail.com>
2021-12-05 17:14:31 -0700

    fixed query apart of and train

--- src/queryTrain.php
+++ src/queryTrain.php
@@ -105,9 +105,10 @@ if(isset($_COOKIE["username"])) {
       printf("error: %d", $err);
     }
 
+
     while($val = mysqli_fetch_array($result))
     {
-      $sql2 = "select name from ROUTES where ID='$rid'";
+      $sql2 = "select name from ROUTES where ID='$val[ID]'";
       $result2 = $conn->query($sql2);
       echo "<tr>";
       echo "<th scope=\"row\">$val[ID]</th>";
