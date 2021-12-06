31745d0981783dcdffa4d29699afc31cd3ae7e96
Riley <riley.weaselfat@uleth.ca>
2021-12-05 23:12:44 -0700

    added time delete

---
+++ src/deleteTime.php
@@ -0,0 +1,28 @@
+<?php
+
+if(isset($_COOKIE["username"])) {
+  $username = $_COOKIE["username"];
+  $password = $_COOKIE["password"];
+  $conn = new mysqli("vconroy.cs.uleth.ca",$username,$password,$username);
+  $val = $_GET['id'];
+  $sql = "delete from TIMES where ID='$val'";
+  if($conn->query($sql)) {
+    echo "<h3>Time Deleted</h3>";
+    echo "<br><a href=\"delete_Time.php\">Return</a> back to Time delete Page";
+  } else {
+    $err = $conn->errno;
+    $errtext = $conn->error;
+    if($err == 1451) {
+      echo "<h3>Cannot delete TIME $_GET[name]!</h3>";
+      echo "Something happened and cannot delete $_GET[id]!";
+      echo "<br><br><a href=\"delete_Time.php\">Return</a> back to Time delete Page.";
+    } else {
+      echo "You got error code of $err. $errtext";
+      echo "<br><br><a href=\"main.php\">Return</a> to Home Page.";
+    }
+  }
+  echo "<a href=\"main.php\">Return</a> to Home Page.";
+  } else {
+      echo "<h3>You are not logged in!</h3><p> <a href=\"index.php\">Login First</a></p>";
+}
+?>
---
+++ src/delete_Time.php
@@ -0,0 +1,38 @@
+<html>
+  <body>
+      <title> Railroad System - Delete a Train </title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+  <?php
+      if(isset($_COOKIE["username"])) {
+          $username = $_COOKIE["username"];
+          $password = $_COOKIE["password"];
+          $conn = new mysqli("vconroy.cs.uleth.ca",$username,$password,$username);
+          $sql = "select * from TIMES";
+          $result = $conn->query($sql);
+
+          echo "<table class=\"table table-striped table-hover\">";
+          echo "<thead><tr>";
+          echo "<th scope=\"col\">Station</th>";
+          echo "<th scope=\"col\">Arrivals</th>";
+          echo "<th scope=\"col\">Delete</th>";
+          echo "</tr></thead>";
+          echo "<tbody>";
+          while($val = mysqli_fetch_array($result)) {
+            echo "<tr>";
+            echo "<th scope=\"row\">$val[ID]</th>";
+            echo "<td>$val[arrivals]</td>";
+            echo "<td><a href=\"deleteTime.php?id=$val[ID]\">Delete</a></td>";
+            echo "</tr>";
+          }
+          echo "</tbody>";
+          echo "</table>";
+          echo "<a href=\"main.php\">Return</a> to Home Page.";
+        } else {
+          echo "<h1>Not logged in redirecting...</h1>";
+          header("Location: index.php");
+          die();
+        }
+  ?>
+</html>
